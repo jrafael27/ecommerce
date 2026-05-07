911626e63fe334d05629e55b9e677098f35fb783
jrafael27 <joshuarafael888@gmail.com>
2026-05-07 19:26:32 +0700

    fix: install missing dependencies

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
 // export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-export const getStripe = () => {
+export const stripe = () => {
   return new Stripe(process.env.STRIPE_SECRET_KEY!);
 };

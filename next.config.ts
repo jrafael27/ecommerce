050341af33f08449bd218bf24088b4548f932554
jrafael27 <joshuarafael888@gmail.com>
2026-05-07 20:45:45 +0700

    fix: install missing dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images:{
-    domains: ["files.stripe.com"]
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "files.stripe.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
